12016d44a6b90860d217cfe7fbb46eb286c8968d
mony32155-del <mony32155@gmail.com>
2025-10-20 20:39:33 +0600

    update

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Demo() {
   return (
-    <div>
-      <h1>Demo Page</h1>
+    <div className="w-full flex between justify-center text-center">
+      <h2 className="text-red-600 text-2xl text-center border-gray-800">
+        This is demo page.
+      </h2>
     </div>
   );
 }
